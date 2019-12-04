15d314edfcb48dcc2e502155bc05378d334915e5
Lijuan Hou <lijuan.hou@sjsu.edu>
2019-12-03 22:16:15 -0800

    fix image bug

--- companies/lijuan/dirty-tea.php
+++ companies/lijuan/dirty-tea.php
@@ -1,6 +1,6 @@
 <?php include 'header.php';?>
 <div>
-<img with="400" height="400" src="<?php echo $path;?>images/lijuan/4.jpg">
+<img with="400" height="400" src="<?php echo $path;?>images/lijuan/4.png">
 </div>
 <div>
 The so-called "dirty tea", "dirty" is actually the gorgeous and cool tiger pattern on the cup wall, which is the effect of milk pouring into brown sugar instantly. Only fresh milk and now thick brown sugar can form, giving a dirty vision, and this "tiger" is the soul of dirty tea.
