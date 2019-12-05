9e6e360309c454161873287a14e09eba4e56cf73
Ru Zhang <rzhang85@asu.edu>
2019-12-05 00:55:40 -0800

    Update index.php
    update 10 product in index

--- index.php
+++ index.php
@@ -529,7 +529,7 @@
                             </a>
                         </div>
                     </div> <!-- /item -->
-                    
+
                     <div class="bgrid item">
                         <div class="item-wrap">
                             <a href="companies/junyanwang/Meet the Toys in Toy Story Land.php">
@@ -554,7 +554,7 @@
                                 </div>
                             </a>
                         </div>
-                    </div> <!-- /item -->                                        
+                    </div> <!-- /item -->
 
                     <div class="bgrid item">
                         <div class="item-wrap">
@@ -567,7 +567,7 @@
                                 </div>
                             </a>
                         </div>
-                    </div> <!-- /item -->  
+                    </div> <!-- /item -->
 
                     <div class="bgrid item">
                         <div class="item-wrap">
@@ -580,7 +580,7 @@
                                 </div>
                             </a>
                         </div>
-                    </div> <!-- /item --> 
+                    </div> <!-- /item -->
 
                     <div class="bgrid item">
                         <div class="item-wrap">
@@ -769,6 +769,167 @@
 
     <!-- Ru's Company -->
     <!-- TODO: #5 10 product pages per persoin -->
+
+    <section id="portfolio">
+        <div class="row section-head">
+            <div class="twelve columns about">
+                <h1>Top 10 Switch Console Game of all time<span>.</span></h1>
+                <p>Below is the list of Top 10 Switch Console Game.</p>
+                <br>
+            </div>
+        </div>
+
+        <div class="row items">
+            <div class="twelve columns">
+
+                <div id="portfolio-wrapper" class="bgrid-fourth s-bgrid-third mob-bgrid-half group">
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Mario Kart 8 Deluxe.php">
+                                <img src="images/ruzhang/Mario Kart 8 Deluxe.png" alt="Mario Kart 8 Deluxe">
+                                <div class="overlay"></div>
+                                <div class="portfolio-item-meta">
+                                    <h5>Mario Kart 8 Deluxe</h5>
+                                    <p>No.1</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Super Smash Bros Ultimate.php">
+                                <img src="images/ruzhang/Super Smash Bros Ultimate.png" alt="Super Smash Bros. Ultimate">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Super Smash Bros. Ultimate</h5>
+                                        <p>No.2</p>
+                                    </div>
+                                </div>
+                            </a>
+
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid stack item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Super Mario Odyssey.php">
+                                <img src="images/ruzhang/Super Mario Odyssey.png" alt="Super Mario Odyssey">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Super Mario Odyssey</h5>
+                                        <p>No.3</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/The Legend of Zelda BOTW.php">
+                                <img src="images/ruzhang/The Legend of Zelda BOTW.png" alt="The Legend of Zelda BOTW">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>The Legend of Zelda: BOTW</h5>
+                                        <p>No.4</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Pokemon Lets Go Pikachu.php">
+                                <img src="images/ruzhang/Pokemon Lets Go Pikachu.png" alt="Pokemon Lets Go Pikachu">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Pokemon Let's Go Pikachu</h5>
+                                        <p>No.5</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Splatoon 2.php">
+                                <img src="images/ruzhang/Splatoon 2.png" alt="Splatoon 2">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Splatoon 2</h5>
+                                        <p>No.6</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Super Mario Party.php">
+                                <img src="images/ruzhang/Super Mario Party.png" alt="webDevelopment">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Super Mario Party</h5>
+                                        <p>No.7</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- /item -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/New super Mario Bros U Deluxe.php">
+                                <img src="images/ruzhang/New super Mario Bros U Deluxe.png" alt="New super Mario Bros. U Deluxe">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>New super Mario Bros. U Deluxe</h5>
+                                        <p>No.8</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- item end -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/Super Mario Maker 2.php">
+                                <img src="images/ruzhang/Super Mario Maker 2.png" alt="Super Mario Maker 2">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>Super Mario Maker 2</h5>
+                                        <p>No.9</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- item end -->
+
+                    <div class="bgrid item">
+                        <div class="item-wrap">
+                            <a href="companies/ruzhang/The Legend of Zelda Links Awakening.php">
+                                <img src="images/ruzhang/The Legend of Zelda Links Awakening.png" alt="The Legend of Zelda: Link's Awakening">
+                                <div class="overlay">
+                                    <div class="portfolio-item-meta">
+                                        <h5>The Legend of Zelda: Link's Awakening</h5>
+                                        <p>No.10</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> <!-- item end -->
+
+                </div> <!-- /portfolio-wrapper -->
+
+            </div> <!-- /twelve -->
+        </div> <!-- /row -->
+    </section> <!-- /Portfolio -->
+
     <!-- The end of Ru's Company -->
 
     <!-- Services Section
