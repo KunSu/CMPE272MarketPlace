645f007ae42510b1105654b9e7d88c406490725b
Yixuan Yang <yixuan.yang@sjsu.edu>
2019-12-09 01:20:10 -0800

    addViewsTODB and getViewsFromDB
    Finish and tested addViewsTODB and getViewsFromDB function on ios.php page.
    1. tested with local database followed setting from the doc file.
    2. able to add viewCount to database
    3. replaced the old get_product_view_count(); to get_top_five();

---
+++ productsViewDB.php
@@ -0,0 +1,57 @@
+<?php
+ define('DB_SERVER', 'localhost');
+ define('DB_USERNAME', 'dev');
+ define('DB_PASSWORD', '123456');
+ define('DB_NAME', 'market_place');
+
+ 
+function update_view_count_productDB($product_name) {
+     
+    /* Attempt to connect to MySQL database */
+    $db_connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    
+    // Check connection
+    if($db_connection === false){
+        die("ERROR: Could not connect to the Database." . mysqli_connect_error());
+    }
+    
+    if(!isset($_COOKIE["product_view_count"])) {
+        echo "Cookie is not set.";
+        $result = "UPDATE product SET viewCount = 0";
+    }else {        
+        $result = "UPDATE product SET viewCount = viewCount + 1 WHERE name = '$product_name' ";
+    }
+
+    if (mysqli_query($db_connection, $result)) {
+        //Testing purpose
+        //echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($db_connection);
+    }
+    
+    mysqli_close($db_connection);
+}
+
+function get_top_five(){
+    $db_connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if(isset($_COOKIE["product_view_count"])) {
+
+        $result = mysqli_query($db_connection,"SELECT name, viewCount FROM product WHERE viewCount > 0 ORDER BY viewCount DESC");
+        echo '<aside class="product">';
+        echo "<p>Last five most visited pages from all time</p>";
+        $index = 0;
+        for ($index = 0; $index <5; $index++) {
+            if (($query_data = mysqli_fetch_array($result))){
+                echo "<br><a href=" . $query_data['name']. ".php>" . $query_data['name'] . ": " . $query_data['viewCount'] . " views</a><br>";
+            }
+        }
+        echo "</aside>";
+    }
+    
+    mysqli_close($db_connection);
+
+}
+
+
+?>
