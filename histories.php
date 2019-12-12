b16164e4cb3603987793c3e7fa913f1179435ff3
lijuan Hou <lijuan.hou@gmail.com>
2019-12-11 22:46:45 -0800

    add sub histories

--- histories.php
+++ histories.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 // check session
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_SESSION["id"])){
     header("location: login.php");
@@ -11,18 +10,35 @@ require_once "config.php";
 $userid = $_SESSION["id"];
 
 //get last 5
-$sql = "SELECT item, max(created_at) FROM histories where userid = ".$userid." GROUP BY item ORDER BY max(created_at) DESC LIMIT 5";
+$sql = "SELECT item,company, max(created_at) FROM histories where userid = ".$userid." GROUP BY item, company ORDER BY max(created_at) DESC LIMIT 5";
 
+echo "<h2>Hostories:</h2><div>";
 $histories = array();
 $result = $db_connection->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-      echo "<div><a href=\"".$row["item"]."\">".getName($row["item"])."</a></div>";
+      echo "<div><a href=\"/companies/".$row["company"]."/".$row["item"].".php\">".$row["item"]."</a></div>";
     }
 }
+echo "</div>";
 
-function getName($path) {
-  $start = strpos($path,"/",11);
-  return rawurldecode(substr($path, $start+1, -4));
+
+$companies = array("lijuan", "kunsu", "tyang", "junyanwang", "zijian", "ruzhang");
+foreach($companies as $company) {
+  getHistoryOf($company, $userid);
+}
+
+function getHistoryOf($company, $userid) {
+  global $db_connection;
+  $sql = "SELECT item, max(created_at) FROM histories where userid = ".$userid." AND company = '".$company."' GROUP BY item ORDER BY max(created_at) DESC LIMIT 5";
+
+  $result = $db_connection->query($sql);
+  if ($result->num_rows > 0) {
+    echo "<h3>History of ".$company.":</h3>";
+    while($row = $result->fetch_assoc()) {
+      echo "<div><a href=\"/companies/".$company."/".$row["item"].".php\">".$row["item"]."</a></div>";
+    echo "</div>";
+    }
+  }
 }
 ?>
--- updateLastFiveHeader.php
+++ updateLastFiveHeader.php
@@ -10,10 +10,20 @@ $item = $_SERVER["REQUEST_URI"];
 
 require_once "config.php";
 
-$sql = "INSERT into histories(userid, item, created_at) VALUE (".$userid.",'".$item."', NOW())";
-
+$sql = "INSERT into histories(userid, item, company,created_at) VALUE (".$userid.",'".getName($item)."','".getCompany($item)."', NOW())";
+echo $sql;
 if ($db_connection->query($sql) !== TRUE) {
   echo $db_connection->error;
 }
 
+
+function getCompany($path) {
+  $len = strpos($path, "/", 11);
+  return rawurldecode(substr($path, 11, $len-11));
+}
+
+function getName($path) {
+  $start = strpos($path,"/",11);
+  return rawurldecode(substr($path, $start+1, -4));
+}
 ?>
