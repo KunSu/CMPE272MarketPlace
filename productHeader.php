0d4143806bde9d0a3be668618f6bbaf63c445858
Ru Zhang <rzhang85@asu.edu>
2019-12-14 17:50:14 -0800

    config.php reuse

--- productHeader.php
+++ productHeader.php
@@ -1,7 +1,9 @@
-<?php $path = "../../";?>
+<?php $path = "../../";
+
+?>
 
 <div id="top"></div>
-<div id="preloader"> 
+<div id="preloader">
     <div id="status">
         <img src="images/loader.gif" height="60" width="60" alt="">
         <div class="loader">Loading...</div>
@@ -16,8 +18,8 @@
             <a href="<?php echo $path;?>index.php">Puremedia.</a>
         </div>
 
-        <nav id="nav-wrap">         
-            
+        <nav id="nav-wrap">
+
             <a class="mobile-btn" href="<?php echo $path;?>#nav-wrap" title="Show navigation">
             <span class='menu-text'>Show Menu</span>
             <span class="menu-icon"></span>
@@ -25,18 +27,18 @@
         <a class="mobile-btn" href="<?php echo $path;?>#" title="Hide navigation">
             <span class='menu-text'>Hide Menu</span>
             <span class="menu-icon"></span>
-        </a>         
+        </a>
 
             <ul id="nav" class="nav">
             <li><a href="<?php echo $path;?>index.php#hero">Home.</a></li>
                 <li class="current"><a href="<?php echo $path;?>index.php#portfolio">Works.</a></li>
             <li><a href="<?php echo $path;?>index.php#services">Services.</a></li>
             <li><a href="<?php echo $path;?>index.php#about">About.</a></li>
-            <li><a href="<?php echo $path;?>index.php#journal">Blog.</a></li>	        
+            <li><a href="<?php echo $path;?>index.php#journal">Blog.</a></li>
             <li><a href="<?php echo $path;?>index.php#contact">Contact.</a></li>
-            </ul> 
+            </ul>
 
-        </nav> <!-- /nav-wrap -->	      
+        </nav> <!-- /nav-wrap -->
 
     </div> <!-- /header-inner -->
 
