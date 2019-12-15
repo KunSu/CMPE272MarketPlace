8a63867a17021ff03abc9b6e0f2a39ea1f656b73
KunSu <kunsu.903@gmail.com>
2019-12-14 21:58:59 -0800

    Change title

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
                         <!-- Slide -->
                         <li>
                             <div class="flex-caption">
-                                <h1>Welcome to Market Place.</h1>
+                                <h1>Welcome to Team Wonderful's Market Place.</h1>
                                 <p><a class="button stroke smoothscroll" href="#about">More About Us</a></p>
                             </div>
                         </li>
