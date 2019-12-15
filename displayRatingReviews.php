5141e3d42391417033ac0a35dc62640235d78ca1
Ru Zhang <rzhang85@asu.edu>
2019-12-14 16:56:02 -0800

    Update Display

--- displayRatingReviews.php
+++ displayRatingReviews.php
@@ -5,7 +5,7 @@
     $productID_query = mysqli_fetch_array($productID_query);
     $productID = $productID_query['id'];
 
-    $sql = "SELECT * FROM productreviews WHERE productID = '$productID'";
+    $sql = "SELECT * FROM productreviews WHERE productID = '$productID' ORDER BY created_at DESC";
     $result = mysqli_query($db_connection, $sql);
 
 ?>
@@ -27,21 +27,26 @@
                   echo "<h1>Comment Section</h1>";
                   echo "<thead>";
                   echo "<tr>";
-                  echo "<th scope=\"col\">userID:</th>";
+                  echo "<th scope=\"col\">user:</th>";
                   echo "<th scope=\"col\">created_at:</th>";
                   echo "<th scope=\"col\">rating:</th>";
                   echo "<th scope=\"col\">review:</th>";
                   echo "</tr>";
                   echo" </thead>";
                 }
-                
+
                 while($row = mysqli_fetch_array($result))
                 {
                     echo "<tr>";
-                    echo '<td style="text-align:center;">' . $row['userID'] . '</td>';
+                    echo '<td style="text-align:center;">' . $row['userName'] . '</td>';
                     echo '<td style="text-align:center;">' . $row['created_at'] . "</td>";
                     echo '<td style="text-align:center;">' . $row['rating'] . "</td>";
-                    echo '<td style="text-align:center;">' . $row['review'] . "</td>";
+                    if(strlen($row['review']) > 100)
+                      echo '<td style="text-align:center;">' . substr($row['review'], 0, 100) . " ...</td>";
+                    else
+                      echo '<td style="text-align:center;">' . $row['review'] . "</td>";
+
+
                     echo "</tr>";
                 }
                 mysqli_close($db_connection);
--- rate.php
+++ rate.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 
-if(isset($_GET['rating'] ,$_GET['name'], $_GET['comment']))
+if(isset($_GET['rating'] ,$_GET['name'], $_GET['comment'], $_COOKIE['userID']))
 {
 	//get the rating
 	$rating = $_GET['rating'];
@@ -14,21 +14,29 @@ if(isset($_GET['rating'] ,$_GET['name'], $_GET['comment']))
 	$productID = $productID_query['id'];
 
 	//get the user information
-	//TODO...
-	$userID = 321; //fake testing data
+	$userID = $_COOKIE['userID'];
+
+	// get user name information
+	$userName_sql = "SELECT username FROM users WHERE id = '$userID'";
+	$userName_query = mysqli_query($db_connection, $userName_sql);
+	$userName_query = mysqli_fetch_array($userName_query);
+	$userName = $userName_query['username'];
+
+	echo $userName;
 
 	//get the review description
 	$review = $_GET['comment'];
 
 	//insert new record into product review
-	$productreviews_sql = "INSERT INTO productreviews (productID, userID, rating, review) VALUES ('$productID', '$userID', '$rating', '$review')";
+	$productreviews_sql = "INSERT INTO productreviews (productID, userID, userName , rating, review) VALUES ('$productID', '$userID', '$userName', '$rating', '$review')";
 
 	$stmt = mysqli_prepare($db_connection, $productreviews_sql);
 	mysqli_stmt_execute($stmt);
 	mysqli_stmt_close($stmt);
 	mysqli_close($db_connection);
 	$redirect =$name . ".php";
-	//TODO: handle redirect
+
+	// handle redirect
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 }
