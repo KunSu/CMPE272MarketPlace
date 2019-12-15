72355cafab69d9dba44c469af27aec6bc404a632
KunSu <kunsu.903@gmail.com>
2019-12-14 20:22:31 -0800

    Add User Page

---
+++ getUser.php
@@ -0,0 +1,18 @@
+<?php
+// Include config file
+require_once "config.php";
+
+// process get request
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    
+    $sql = "SELECT username, email, homephone FROM users";
+    $sth = mysqli_query($db_connection, $sql);
+    $result = array();
+    while($r = mysqli_fetch_assoc($sth)) {
+        $result[] = $r;
+    }
+    $json = json_encode($result, true);
+    print_r($json);
+}
+
+?>
